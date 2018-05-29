72597dbbe6fd3e3a7ca7a0479a30b79f55b8b642
Junian Triajianto <author@junian.net>
2018-05-29 14:59:17 +0700

    Add zxing net mobile to core

--- src/FactorAuth.Core/Services/IQRScanner.cs
+++ src/FactorAuth.Core/Services/IQRScanner.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Threading.Tasks;
+
 namespace FactorAuth.Core.Services
 {
     public interface IQRScanner
     {
+        
+        Task ScanFromCameraAsync();
     }
 }
